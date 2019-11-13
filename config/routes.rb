370a999c83eb3d062f403d9960d9859c0607cb09
Aleem Pathammavong <Aleemp321@yahoo.com>
2019-11-13 16:29:07 -0700

    basic obj complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  resources :departments 
+root 'static_pages#home'
+  
+  resources :departments do
+    resources :items
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_210356) do
+ActiveRecord::Schema.define(version: 2019_11_13_214323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 2019_11_13_210356) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.bigint "department_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["department_id"], name: "index_items_on_department_id"
+  end
+
+  add_foreign_key "items", "departments"
 end
